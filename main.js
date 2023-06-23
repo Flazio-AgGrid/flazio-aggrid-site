7fdc760906579008b686e2c75c08102bb3d9aa10
Q300Z <Q300Zhomas@Gmail.com>
2023-06-23 11:45:17 +0200

    Resize automatique des colonnes

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 const gridOptions = {
   columnDefs: [
-    { field: 'id', filter: 'agTextColumnFilter' },
-    { field: 'AccountID' },
-    { field: 'TaxRegID', minWidth: 180 },
-    { field: 'Nome', minWidth: 180 },
-    { field: 'Cognome', minWidth: 150 },
-    { field: 'RagioneSociale', minWidth: 250 },
-    { field: 'Email' },
-    { field: 'Indirizzo', minWidth: 250 },
-    { field: 'Comune' },
-    { field: 'CAP', },
-    { field: 'Provincia' },
-    { field: 'Tel', minWidth: 180 },
-    { field: 'NumMobile' },
-    { field: 'NumPagineDaAttivare', minWidth: 150 },
-    { field: 'NumPagineAttivate' },
-    { field: 'status' },
-    { field: 'create_dt', minWidth: 200 },
-    { field: 'update_dt', minWidth: 200 },
-    { field: 'readed' },
-    { field: 'reseller_experience_manager_id' },
-    { field: 'CAOName' },
+    { field: "id", filter: "agTextColumnFilter" },
+    { field: "AccountID" },
+    { field: "TaxRegID" },
+    { field: "Nome" },
+    { field: "Cognome" },
+    { field: "RagioneSociale" },
+    { field: "Email" },
+    { field: "Indirizzo" },
+    { field: "Comune" },
+    { field: "CAP" },
+    { field: "Provincia" },
+    { field: "Tel" },
+    { field: "NumMobile" },
+    { field: "NumPagineDaAttivare" },
+    { field: "NumPagineAttivate" },
+    { field: "status" },
+    { field: "create_dt" },
+    { field: "update_dt" },
+    { field: "readed" },
+    { field: "reseller_experience_manager_id" },
+    { field: "CAOName" },
   ],
   defaultColDef: {
-    flex: 1,
-    minWidth: 100,
     enableValue: true,
     enableRowGroup: true,
     enablePivot: true,
@@ -32,22 +30,26 @@ const gridOptions = {
     filter: true,
     resizable: true,
   },
-  autoGroupColumnDef: {
-    minWidth: 200,
-  },
   sideBar: true,
 };
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  var gridDiv = document.querySelector('#myGrid');
+document.addEventListener("DOMContentLoaded", function () {
+  var gridDiv = document.querySelector("#myGrid");
   new agGrid.Grid(gridDiv, gridOptions);
 
-  // fetch('data.json')
-  //   .then((response) => response.json())
-  //   .then((data) => gridOptions.api.setRowData(data));
-  fetch('backend.php')
+  fetch("backend.php")
     .then((response) => response.json())
-    .then((data) => gridOptions.api.setRowData(data));
+    .then((data) => {
+      gridOptions.api.setRowData(data);
+      autoSizeAll();
+    });
 });
+
+function autoSizeAll(skipHeader) {
+  const allColumnIds = [];
+  gridOptions.columnApi.getColumns().forEach((column) => {
+    allColumnIds.push(column.getId());
+  });
+
+  gridOptions.columnApi.autoSizeColumns(allColumnIds, skipHeader);
+}
