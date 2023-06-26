96a416767fdba3d52aa7b3772ef14bce3278ed4e
Q300Z <Q300Zhomas@Gmail.com>
2023-06-26 14:04:58 +0200

    Add static colonme

--- backend.php
+++ backend.php
@@ -7,38 +7,55 @@ if ($mysqli->connect_errno) {
     echo "Échec de la connexion MySQL : " . $mysqli->connect_error;
     exit();
 }
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $query = "SELECT r.*, m.id_cat, m.id_cat_automatica FROM reseller_experience_customer r LEFT JOIN maps_info m ON r.id = m.fk_lead WHERE m.id_cat IS NULL LIMIT 10;";
 
-$query = "SELECT r.*
-    FROM reseller_experience_customer r
-    LEFT JOIN maps_info m ON r.id = m.fk_lead
-    WHERE m.id_cat IS NULL
-    LIMIT 10";
+    $result = $mysqli->query($query);
 
-$result = $mysqli->query($query);
+    // Vérifier si la requête a échoué
+    if (!$result) {
+        echo "Erreur lors de l'exécution de la requête : " . $mysqli->error;
+        exit();
+    }
 
-// Vérifier si la requête a échoué
-if (!$result) {
-    echo "Erreur lors de l'exécution de la requête : " . $mysqli->error;
-    exit();
-}
+    // Créer un tableau pour stocker les données
+    $data = array();
 
-// Créer un tableau pour stocker les données
-$data = array();
+    // Traiter les résultats de la requête
+    while ($row = $result->fetch_assoc()) {
+        // Ajouter chaque ligne de résultat au tableau
+        $data[] = $row;
+    }
 
-// Traiter les résultats de la requête
-while ($row = $result->fetch_assoc()) {
-    // Ajouter chaque ligne de résultat au tableau
-    $data[] = $row;
-}
+    // Convertir le tableau en JSON
+    $jsonData = json_encode($data);
 
-// Convertir le tableau en JSON
-$jsonData = json_encode($data);
-
-// Enregistrer le JSON dans un fichier
+    // Enregistrer le JSON dans un fichier
 // $file = 'data.json';
 // file_put_contents($file, $jsonData);
-echo $jsonData;
-//echo "Les données ont été transférées vers un fichier JSON avec succès.";
+    echo $jsonData;
+    //echo "Les données ont été transférées vers un fichier JSON avec succès.";
+}
+else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $jsonData     = file_get_contents('php://input');
+    $modifiedData = json_decode($jsonData, true);
+
+    foreach ($modifiedData["modifiedData"] as $row) {
+        $id     = $row['id'];
+        $id_cat = $row['id_cat'];
+
+        $sql = "UPDATE reseller_experience_customer r LEFT JOIN maps_info m ON r.id = m.fk_lead SET m.id_cat = $id_cat WHERE r.id = $id";
+
+        if ($mysqli->query($sql) === TRUE) {
+            $response = array("message" => "Mise a jour reussie pour l'enregistrement avec l'ID : " . $id, "sql" => $sql);
+            echo json_encode($response);
+        }
+        else {
+            $response = array("message" => "Erreur lors de la mise a jour pour l'enregistrement avec l'ID : " . $id, "erreur" => $mysqli->error);
+            echo json_encode($response);
+        }
+    }
+}
 
 // Fermer la connexion à la base de données
 $mysqli->close();
--- main.js
+++ main.js
@@ -21,6 +21,17 @@ const gridOptions = {
     { field: "readed" },
     { field: "reseller_experience_manager_id" },
     { field: "CAOName" },
+    {
+      field: "id_cat",
+      headerName: "Category choice",
+      pinned: "right",
+      editable: true,
+      cellEditor: "agRichSelectCellEditor",
+      cellEditorPopup: true,
+      cellEditorParams: {
+        values: categoryValues(),
+      },
+    },
   ],
   defaultColDef: {
     enableValue: true,
@@ -40,10 +51,19 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("backend.php")
     .then((response) => response.json())
     .then((data) => {
+      setCategory(data);
       gridOptions.api.setRowData(data);
       autoSizeAll();
       gridOptions.api.closeToolPanel();
     });
+
+  // Ajouter un gestionnaire d'événements pour écouter les modifications de cellule
+  gridOptions.api.addEventListener("cellValueChanged", function (event) {
+    // Récupérer les données modifiées
+    var modifiedData = [event.data];
+    // Envoyer les données modifiées au backend pour enregistrement
+    saveChangesToBackend(modifiedData);
+  });
 });
 
 function autoSizeAll(skipHeader) {
@@ -53,4 +73,70 @@ function autoSizeAll(skipHeader) {
   });
 
   gridOptions.columnApi.autoSizeColumns(allColumnIds, skipHeader);
+}
+
+function categoryValues() {
+  const category = [
+    "2 - Personal website or Freelancer",
+    "130 - Business",
+    "137 - Blog",
+    "125 - Services",
+    "44 - Booking",
+    "68 - Restaurant",
+    "8 - Design ",
+    "66 - Advertising",
+    "1 - Real Estate",
+    "37 - Health and Beatuy",
+    "78 - Portfolio",
+  ];
+
+  return category;
+}
+
+function setCategory(data) {
+  const category = categoryValues();
+  const data_tmp = data;
+  data_tmp.map((el) => {
+    console.log(el.id_cat_automatica, el.id_cat);
+    if (el.id_cat_automatica !== null) {
+      el.id_cat = category[el.id_cat_automatica];
+    } else if (el.id_cat !== null) {
+      el.id_cat = category[el.id_cat];
+    }
+  });
+}
+
+function saveChangesToBackend(data) {
+  // Envoyer les données modifiées au backend pour enregistrement
+  // Utilisez ici votre logique d'enregistrement des modifications
+  var category = categoryValues();
+  var data_tmp = data;
+  category.map((el) => {
+    if (el === data_tmp[0].id_cat) {
+      data_tmp[0].id_cat = category.indexOf(el);
+    }
+  });
+  console.log("Données modifiées :", data_tmp);
+  fetch("backend.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ modifiedData: data_tmp }),
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result); // Afficher la réponse du backend
+      body = document.querySelector("body");
+      out = document.createElement("div");
+      out.innerHTML = result;
+      body.appendChild(out);
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'envoi des données au backend:", error);
+      body = document.querySelector("body");
+      out = document.createElement("div");
+      out.innerHTML = error;
+      body.appendChild(out);
+    });
 }
